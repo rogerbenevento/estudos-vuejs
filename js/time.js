a6c048a98c005ec3a894fd8efc188e314c01ec80
Rogerio Benevento Jr <rogerbenevento@gmail.com>
2019-07-05 18:41:06 -0300

    Estudos de vuejs

---
+++ js/time.js
@@ -0,0 +1,11 @@
+class Time {
+    constructor(nome, escudo) {
+        this.nome = nome
+        this.escudo = escudo
+
+        this.pontos = 0
+        this.gm = 0
+        this.gs = 0
+        this.pontos = 0
+    }
+}
